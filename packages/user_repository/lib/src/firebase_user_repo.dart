5b8043e6d444cd5ebd86f14862514339994dd045
Naaman <makafui17@gmail.com>
2024-06-25 13:57:20 -0400

    Implemented authentication v1 flow.

---
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -0,0 +1,79 @@
+import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:user_repository/src/entities/entities.dart';
+import 'package:user_repository/src/models/users.dart';
+import 'package:user_repository/src/user_repo.dart';
+
+
+class FirebaseUserRepo implements UserRepository {
+  
+  final FirebaseAuth _firebaseAuth;
+  final usersCollection = FirebaseFirestore.instance.collection('users');
+
+  FirebaseUserRepo({
+    FirebaseAuth? firebaseAuth,
+  }) :_firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  @override
+  Future<void> logout() async{
+    await _firebaseAuth.signOut();
+  }
+
+  @override
+  Future<void> setUserData(MyUser user) async{
+    try {
+      await usersCollection.doc(user.userId)
+          .set(user.toEntity().toJsonDocument());
+    }catch(e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> signIn(String email, String password) async {
+    
+    try{
+      await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    }catch(e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<MyUser> signUp (MyUser myUser, String password) async{
+
+    try{
+      UserCredential userCredentials = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: myUser.email, 
+        password: password);
+      
+      myUser.userId = userCredentials.user!.uid;
+      return myUser;
+    }catch(e) {
+      log(e.toString());
+      rethrow;
+    }
+
+  }
+
+  @override
+  Stream<MyUser?> get user {
+
+    return _firebaseAuth.authStateChanges().flatMap((firebaseUser) async* {
+      if (firebaseUser == null) {
+        yield MyUser.empty;
+      }else {
+        yield await usersCollection
+          .doc(firebaseUser.uid)
+          .get()
+          .then((value) => MyUser.fromEntity(MyUserEntity.fromJsonDocument(value.data()!)));
+      }
+    });
+
+  }
+  
+}
